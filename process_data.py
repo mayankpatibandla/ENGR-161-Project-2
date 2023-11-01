50b4d043811dfcf58848be60fe9376fc2273c837
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-11-01 00:48:54 -0400

    Condense

--- process_data.py
+++ process_data.py
@@ -1,61 +1,47 @@
 from csv import DictReader
 from json import dump, load
 
-data = {}
+JSON_FILE = "equipment.json"
 
 
-def foo(file):
-    with open(f"data/{file}.csv", encoding='utf-8', newline='') as f:
-        x = list(DictReader(f))
-        power = x[0]
-        efficiency = x[1]
-        cost = x[2]
+def generate_json() -> None:
+    data = {}
+    for file in ["fermenters", "distillers", "filters"]:
+        with open(f"data/{file}.csv", encoding='utf-8', newline='') as f:
+            x = list(DictReader(f))
+            power = x[0]
+            efficiency = x[1]
+            cost = x[2]
 
-        result = {}
-        for key in x[0].keys():
-            result[key] = {
-                "power": float(power[key]),
-                "efficiency": float(efficiency[key]),
-                "cost": float(cost[key]),
-            }
+            result = {}
+            for key in x[0].keys():
+                result[key] = {
+                    "power": float(power[key]),
+                    "efficiency": float(efficiency[key]),
+                    "cost": float(cost[key]),
+                }
+            data[file] = result
+    for file in ["pumps", "pipes", "ductworks", "bends", "valves"]:
+        with open(f"data/{file}.csv", encoding='utf-8', newline='') as f:
+            x = list(DictReader(f))
+            coefficient = x[0]
+            costs = x[1:]
 
-        data[file] = result
+            result = {}
+            for key in x[0].keys():
+                result[key] = {}
+                if key.find("(m)") != -1:
+                    result[key] = [float(cost[key]) for cost in costs]
+                    continue
+                if coefficient[key] != '':
+                    result[key]["coefficient"] = float(coefficient[key])
+                result[key]["cost"] = [float(cost[key]) for cost in costs]
+            data[file] = result
 
-
-def bar(file):
-    with open(f"data/{file}.csv", encoding='utf-8', newline='') as f:
-        x = list(DictReader(f))
-        coefficient = x[0]
-        costs = x[1:]
-
-        result = {}
-        for key in x[0].keys():
-            result[key] = {}
-            if key.find("(m)") != -1:
-                result[key] = [float(cost[key]) for cost in costs]
-                continue
-            if coefficient[key] != '':
-                result[key]["coefficient"] = float(coefficient[key])
-            result[key]["cost"] = [float(cost[key]) for cost in costs]
-
-        data[file] = result
-
-
-FOO_FILES = ["fermenters", "distillers", "filters"]
-BAR_FILES = ["pumps", "pipes", "ductworks", "bends", "valves"]
-JSON_FILE = "equipment"
-
-
-def generate_json():
-    for f in FOO_FILES:
-        foo(f)
-    for f in BAR_FILES:
-        bar(f)
-
-    with open(f"{JSON_FILE}.json", "w", encoding='utf-8') as f:
+    with open(JSON_FILE, "w", encoding='utf-8') as f:
         dump(data, f, indent=2)
 
 
 def load_json() -> dict[str, dict[str, dict[str, list[float] | float]]]:
-    with open(f"{JSON_FILE}.json", "r", encoding='utf-8') as f:
+    with open(JSON_FILE, "r", encoding='utf-8') as f:
         return load(f)
