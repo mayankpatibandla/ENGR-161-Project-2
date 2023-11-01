a39b98ac06b8d782a86eea1d28740b6c5388d2c4
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-11-01 00:43:27 -0400

    Removed raw print

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from argparse import ArgumentParser
 
-from process_data import generate_json, load_json, print_raw
+from process_data import generate_json, load_json
 
 parser = ArgumentParser()
 parser.add_argument("--json", dest="generate_json", help="Generates a JSON file", action="store_true")
-parser.add_argument("--raw", dest="print_raw", help="Prints the raw data", action="store_true")
 parser.add_argument("--print", dest="print_data", help="Prints the JSON data", action="store_true")
 args = parser.parse_args()
 
-data = load_json()
 
 if args.generate_json:
     generate_json()
 
-if args.print_raw:
-    print_raw()
+data = load_json()
 
 if args.print_data:
     print(data)
--- process_data.py
+++ process_data.py
@@ -11,53 +11,18 @@ def foo(file):
         efficiency = x[1]
         cost = x[2]
 
-        print(f"{file.title()}:")
-        for key in x[0].keys():
-            print(f"  {key}:")
-            print(f"    {power[key]}", "kWh/day")
-            print(f"    {efficiency[key]}")
-            print(f"    {cost[key]}", "$ per m^3 / hour of flow")
-        print("\n")
-
-
-def foo2(file):
-    with open(f"data/{file}.csv", encoding='utf-8', newline='') as f:
-        x = list(DictReader(f))
-        power = x[0]
-        efficiency = x[1]
-        cost = x[2]
-
         result = {}
         for key in x[0].keys():
             result[key] = {
-                "power": int(power[key]),
+                "power": float(power[key]),
                 "efficiency": float(efficiency[key]),
-                "cost": int(cost[key]),
+                "cost": float(cost[key]),
             }
 
         data[file] = result
 
 
 def bar(file):
-    with open(f"data/{file}.csv", encoding='utf-8', newline='') as f:
-        x = list(DictReader(f))
-        coefficient = x[0]
-        costs = x[1:]
-
-        print(f"{file.title()}:")
-        for key in x[0].keys():
-            print(f"  {key}:")
-            if key.find("(m)") != -1:
-                for cost in costs:
-                    print(f"    {cost[key]}", "meters")
-                continue
-            print(f"    {coefficient[key]}")
-            for cost in costs:
-                print(f"    {cost[key]}", "$ per ...")
-        print("\n")
-
-
-def bar2(file):
     with open(f"data/{file}.csv", encoding='utf-8', newline='') as f:
         x = list(DictReader(f))
         coefficient = x[0]
@@ -83,21 +48,14 @@ JSON_FILE = "equipment"
 
 def generate_json():
     for f in FOO_FILES:
-        foo2(f)
+        foo(f)
     for f in BAR_FILES:
-        bar2(f)
+        bar(f)
 
     with open(f"{JSON_FILE}.json", "w", encoding='utf-8') as f:
         dump(data, f, indent=2)
 
 
-def load_json():
+def load_json() -> dict[str, dict[str, dict[str, list[float] | float]]]:
     with open(f"{JSON_FILE}.json", "r", encoding='utf-8') as f:
         return load(f)
-
-
-def print_raw():
-    for f in FOO_FILES:
-        foo(f)
-    for f in BAR_FILES:
-        bar(f)
